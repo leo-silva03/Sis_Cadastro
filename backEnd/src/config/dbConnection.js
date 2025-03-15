cf2782851b1b96e1ebc1d2cfe22d6b8590cfb7f7
Leonardo Silva <leonardosergipe03@gmail.com>
2025-03-15 20:03:58 -0300

    Atualização para o Front End

--- backEnd/src/config/dbConnection.js
+++ backEnd/src/config/dbConnection.js
@@ -6,7 +6,7 @@ async function conectaNaDatabase() {
         console.log("Conexão com o banco de dados realizada com sucesso!");
     } catch (error) {
         console.error("Erro ao conectar ao banco de dados:", error);
-        process.exit(1); // Encerra a aplicação em caso de erro crítico
+        process.exit(1); 
     }
     return mongoose.connection;
 }
